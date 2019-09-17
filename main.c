caddeace2e09de30f8978e58ff02df74b4e48eb0
Abhay Ruparel <abhayruparel2000@gmail.com>
2019-09-17 23:59:42 +0530

    CMS: basic displaying functionality
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "string.h"
+void print() {
+	int loginChoice;
+	loginStart:
+	system("cls"); // system("clear"); for linux
+	printf("******Available login options******\n");
+	printf("Press 1 for Owner\nPress 2 for Customer\nPress 3 to exit\n");
+	scanf("%d",&loginChoice);
+	switch(loginChoice) {
+		case 1: printf("Onwer login page");
+				break;
+		case 2: printf("Customer login page:");
+				break;
+		case 3: printf("Thank you!");
+				getchar();
+				break;
+		default: printf("Invalid chocie. Try again.");
+				 print();
+	}
+}
+int main()
+{
+	print();
+	return 0;
+
+}	
