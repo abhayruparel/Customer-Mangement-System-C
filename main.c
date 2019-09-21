ff406b2211c5dbff2ef0fa76f7fdb511c12b89f5
Abhay Ruparel <abhayruparel2000@gmail.com>
2019-09-21 19:14:40 +0530

    CMS: Hold screen for 2s while exiting.. ..clear output buffer in default case
      * Changes Suggested by Mr Aditya Ranjan
    
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
 void print() {
 	int loginChoice;
+	char ch;
 	loginStart:
-	system("clear"); // system("clear"); for linux
+	system("cls"); // system("clear"); for linux
 	printf("******Available login options******\n");
 	printf("Press 1 for Owner\nPress 2 for Customer\nPress 3 to exit\n");
 	scanf("%d",&loginChoice);
@@ -13,10 +15,11 @@ void print() {
 		case 2: printf("Customer login page:");
 				break;
 		case 3: printf("Thank you!");
-				getchar();
+				sleep(3);
 				break;
-		default: printf("Invalid chocie. Try again.\n");
-
+		default: printf("Invalid chocie. Try again.");
+				 //print();
+				 fflush(stdin);
 				 goto loginStart;
 	}
 }
@@ -25,4 +28,4 @@ int main()
 	print();
 	return 0;
 
-}	
+}
